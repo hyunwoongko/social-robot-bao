b3e51d5e1d59e4db4681807c19192538c826ade8
gusdnd852 <gusdnd852@naver.com>
2019-03-09 12:06:42 +0900

    [인공지능] 유사단어추출기 모델 Word2Vec -> Fasttext로 변경

--- chat/model.py
+++ chat/model.py
@@ -1,7 +1,4 @@
-from gensim.models import word2vec
 import pandas as pd
 
 train_data = pd.read_csv('train.csv')
 print(train_data)
-
-
--- chat/word_dict.py
+++ chat/word_dict.py
@@ -1,22 +1,21 @@
 import pandas as pd
-from gensim.models import Word2Vec
+from gensim.models import FastText
 
 input_data = pd.read_csv('voca.csv').values
 word_dict = [sentence[0].split() for sentence in input_data]
 
-model = Word2Vec(word_dict,
-                 size=64,
-                 window=2,
+model = FastText(word_dict,
+                 size=32,
+                 window=3,
                  workers=8,
                  min_count=1,
                  sg=1,
-                 iter=500)
+                 iter=250)
 
 model.init_sims(replace=True)
 
 
 def get_similar_words(word):
-    similarity = model.wv.most_similar(word)
-    similarity_df = pd.DataFrame(similarity, columns=['word', 'similarity'])
-    similar_words = [word[0] for word in similarity_df.values]
+    similarity = model.wv.most_similar(word, topn=5)
+    similar_words = [word[0] for word in similarity]
     return similar_words
