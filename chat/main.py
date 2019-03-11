03fc30729cd6c9f856068e8fbb2a58dad40f0327
gusdnd852 <gusdnd852@naver.com>
2019-03-11 17:06:51 +0900

    [인공지능] 개체명 인식기 롤백

--- chat/main.py
+++ chat/main.py
@@ -1,10 +1,6 @@
-from util.data_utils import Vocabulary # 이거 무조건 있어야함.
-from entity_recognizer import entity_recognize
 from intent_finder import get_intent, intent_mapping, train
 
 train()
 print(intent_mapping)
 question = input()
 print(get_intent(question))
-print(entity_recognize(question))
-
