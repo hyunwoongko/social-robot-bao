90bd54a19a7de65f605d375ae7a81b4b3962d192
Hyeonju Lee <dlguswn77771@naver.com>
2019-03-15 01:24:36 +0900

    [안드로이드] 눈 깜빡임 구현

--- app/src/main/java/com/welfarerobotics/welfareapplcation/MainActivity.java
+++ app/src/main/java/com/welfarerobotics/welfareapplcation/MainActivity.java
@@ -5,20 +5,24 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Handler;
 import android.speech.RecognitionListener;
+import android.speech.RecognitionService;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
@@ -29,11 +33,18 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     private TextToSpeech tts;
     private ImageView img;
     private String speech;
+    private Timer timer;
+    private TimerTask ttListen, ttBlink;
+    private VideoView vv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.txt1);
+        img = findViewById(R.id.face);
+        vv = findViewById(R.id.videoview);
 
         //PERMISSION CHECK
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET,
@@ -43,8 +54,45 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        tv = findViewById(R.id.txt1);
-        img = findViewById(R.id.face);
+/*      //수정중
+        //TIMERTASK(Listening)
+        ttListen = new TimerTask() {
+            @Override
+            public void run() {
+                mRecognizer.destroy();
+                mRecognizer.startListening(i);
+            }
+        };
+*/
+
+        //VIDEO(BLINKING EYES)
+        MediaController mediaController = new MediaController(MainActivity.this);
+        mediaController.setVisibility(View.GONE);
+        vv.setMediaController(mediaController);
+        vv.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.close));
+        vv.start();
+
+        vv.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                img.bringToFront();
+            }
+        });
+
+        //TIMERTASK(VIDEO PLAYBACK)
+        ttBlink = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("video playback");
+                        vv.bringToFront();
+                        vv.start();
+                    }
+                });
+            }
+        };
 
         IntentFilter filter = new IntentFilter();
         filter.addAction(TextToSpeech.ACTION_TTS_QUEUE_PROCESSING_COMPLETED);
@@ -67,7 +115,9 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
 
         @Override
         public void onBeginningOfSpeech() {
-
+            if (timer != null) {
+                timer.cancel();
+            }
         }
 
         @Override
@@ -88,7 +138,12 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         @Override
         public void onError(int error) {
             if (error == SpeechRecognizer.ERROR_SPEECH_TIMEOUT) {
-                mRecognizer.startListening(i);
+                if (timer == null) {
+                    System.out.println("speech timeout");
+                    timer = new Timer();
+                    //  timer.schedule(ttListen, 5000, 10000);
+                    timer.schedule(ttBlink, 7000, 7000);
+                }
             }
         }
 
@@ -96,7 +151,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         public void onResults(Bundle results) {
             ArrayList<String> matches =
                     results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-            speech = matches.get(0).replace(" ", "");
+            speech = matches.get(0).replace(" ", ""); //0번이 가장 다듬어진 문장
             tv.setText(speech);
             tts = new TextToSpeech(MainActivity.this, MainActivity.this);
         }
@@ -143,6 +198,9 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     @Override
     protected void onDestroy() {
         unregisterReceiver(m_br);
+        if (mRecognizer != null) {
+            mRecognizer.destroy();
+        }
         if (tts != null) {
             tts.stop();
             tts.shutdown();
