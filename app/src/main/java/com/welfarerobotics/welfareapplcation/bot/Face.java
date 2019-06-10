fc4dd68d8ff20bafbf0e06de533666ee9c2f25c4
gusdnd852 <gusdnd852@naver.com>
2019-06-10 12:26:33 +0900

    [안드로이드] 불필요한 클래스 삭제

--- app/src/main/java/com/welfarerobotics/welfareapplcation/bot/Face.java
+++
@@ -1,9 +0,0 @@
-package com.welfarerobotics.welfareapplcation.bot;
-
-/**
- * @author : Hyunwoong
- * @when : 5/27/2019 6:34 PM
- * @homepage : https://github.com/gusdnd852
- */
-public class Face {
-}
--- app/src/main/java/com/welfarerobotics/welfareapplcation/bot/Head.java
+++
@@ -1,9 +0,0 @@
-package com.welfarerobotics.welfareapplcation.bot;
-
-/**
- * @author : Hyunwoong
- * @when : 5/26/2019 6:25 PM
- * @homepage : https://github.com/gusdnd852
- */
-public class Head {
-}
